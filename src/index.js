bdb60a14fa28fbcabdf6a473471be861e59d84de
azu <azuciao@gmail.com>
2022-01-29 20:01:27 +0900

    fix: reload when wakeup from sleep

--- src/index.js
+++ src/index.js
@@ -31,9 +31,11 @@ app.on("ready", createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+    app.quit();
+});
+
+app.on("quit", () => {
+    app.quit();
 });
 
 app.on("activate", () => {
